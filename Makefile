aa449c8f82050da6897e6144a989188a23671ed0
Halil Ibrahim Demirci <hademirc@k1m30s11.42istanbul.com.tr>
2025-04-17 04:23:44 +0300

    Finished utils w/ Norminette

--- Makefile
+++ Makefile
@@ -5,7 +5,11 @@ CFLAGS = -Wall -Wextra -Werror
 
 SRC_DIR = src
 UTILS_DIR = utils
-INCLUDES = /printf
+INCLUDES = printf
+
+MLX_DIR		= minilibx-linux
+MLX_LIB		= $(MLX_DIR)/libmlx.a
+MLX_FLAGS	= -lmlx -lXext -lX11 -lm
 
 SRCS =	$(SRC_DIR)/actions.c \
 		$(SRC_DIR)/colors.c \
@@ -17,12 +21,10 @@ SRCS =	$(SRC_DIR)/actions.c \
 
 UTILS = $(UTILS_DIR)/ft_memset.c \
 		$(UTILS_DIR)/ft_strncmp.c \
-		$(UTILS_DIR)$(INCLUDES)/printf.c \
-		$(UTILS_DIR)$(INCLUDES)/printf_utils.c \
+		$(UTILS_DIR)/$(INCLUDES)/printf.c \
+		$(UTILS_DIR)/$(INCLUDES)/printf_utils.c \
 
 OBJS = $(SRCS:.c=.o) $(UTILS:.c=.o)
-
-INCLUDES = -I$(SRC_DIR) -I$(UTILS_DIR)
 
 RM = rm -f
 
@@ -30,15 +32,19 @@ COLOR_MAGENTA		=	\033[0;95m
 COLOR_LIGHT_GREEN   =   \033[0;92m
 
 all: $(NAME)
-$(NAME): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) $(INCLUDES) -o $(NAME)
+$(NAME): $(MLX_LIB) $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -L$(MLX_DIR) $(MLX_FLAGS) -o $(NAME)
 	@echo "$(COLOR_LIGHT_GREEN)*MANDATORY fractol created successfully!"
+
+$(MLX_LIB):
+	make -C $(MLX_DIR)
 
 clean:
 	@echo "$(COLOR_MAGENTA)"
 	$(RM) $(OBJS)
+	make -C $(MLX_DIR) clean
 
 fclean: clean
-	$(RM) $(NAME)
+	$(RM) $(NAME) $(MLX_LIB)
 
 re: fclean all
--- utils/utils.h
+++ utils/utils.h
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hademirc <hademirc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 03:20:47 by hademirc          #+#    #+#             */
+/*   Updated: 2025/04/17 04:00:36 by hademirc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef UTILS_H
-#define UTILS_H
+# define UTILS_H
 
-#include "printf/printf.h"
+# include "printf/printf.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *s, int c, size_t len);
 
 #endif
